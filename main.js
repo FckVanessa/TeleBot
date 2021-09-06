dee75c589a38597358c74bc8957007f7e53f771d
Nino chan <81684610+Nino-chan02@users.noreply.github.com>
2021-09-07 00:46:17 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const axios = require("axios");
-const nhentai = require("nhentai");
 const request = require("request");
-const topdf = require("image-to-pdf");
 const fs = require("fs");
 const os = require("os");
 const chalk = require("chalk");
@@ -233,101 +231,17 @@ bot.command("eval", async (msg) => {
 	}
 })
 
-bot.on('text', async lintod => {
-  let body = lintod.update.message.text || ''
+bot.on('text', async nino => {
+  let body = nino.update.message.text || ''
   let id = body
-  const userName = lintod.message.from.username
+  const userName = nino.message.from.username
 
   // log
   console.log(chalk.whiteBright("├"), chalk.keyword("aqua")("[ TELEBOT ]"), chalk.whiteBright(body), chalk.greenBright("from"), chalk.keyword("yellow")(userName))
 
-  if (body && body.match(/^[0-9]/)) {
-    let count = 0
-    let ResultPdf = [];
-    try {
-      lintod.reply("Please wait, data is being processed");
-      const get_result = new nhentai.API()
-      const doujin = await get_result.fetchDoujin(id);
-      const array_page = doujin.pages
-      const title = doujin.titles.pretty
-      
-      res = await axios.get("http://lolhuman.herokuapp.com/api/nhentai/" + id + "?apikey=https://github.com/BlackAmda")
-      data = res.data.result
-      caption = `${data.title_romaji}\n\n`
-      caption += `${data.title_native}\n\n`
-      caption += `Parodies: ${data.info.parodies}\n\n`
-      caption += `Tags: ${data.info.tags.join(", ")}\n\n`
-      caption += `Artists: ${data.info.artists}\n\n`
-      caption += `Groups: ${data.info.groups}\n\n`
-      caption += `Languages: ${data.info.languages}\n\n`
-      caption += `Categories: ${data.info.categories}\n\n`
-      caption += `Pages: ${data.info.pages}\n\n`
-      caption += `Uploaded: ${data.info.uploaded}\n`
-      await lintod.replyWithPhoto({ url: data.image[0] }, { caption: caption, parse_mode: "Markdown" })
-
-      for (let index = 0; index < array_page.length; index++) {
-        const image_name = "nhentai/" + title + index + ".jpg"
-        await new Promise((resolve) => request(array_page[index]).pipe(fs.createWriteStream(image_name)).on('finish', resolve))
-        console.log(array_page[index].url);
-
-        ResultPdf.push(image_name);
-        count++
-      }
-
-      await new Promise((resolve) =>
-        topdf(ResultPdf, 'A4')
-        .pipe(fs.createWriteStream('nhentai/' + title + '.pdf'))
-        .on('finish', resolve)
-      )
-
-      for (let index = 0; index < array_page.length; index++) {
-        fs.unlink("nhentai/" + title + index + ".jpg", (err) => {
-          if (err) throw err
-        })
-      }
-
-      const size = await fs.statSync(`nhentai/${title}.pdf`).size
-      if (size < 45000000) {
-        lintod.reply("Uploading document...");
-        await lintod.replyWithDocument({
-          source: `nhentai/${title}.pdf`, filename: `${title}.pdf`
-        })
-        .then((result) => {
-          fs.unlink(`nhentai/${title}.pdf`, (err) => {
-            if (err) throw err
-          })
-        })
-        .catch((erro) => {
-          console.error("Error when sending: ",
-            erro);
-          lintod.reply(String(erro));
-        })
-      } else {
-        lintod.reply("Uploading to anonfiles because file size to large for Telegram");
-        const options = {
-          method: "POST",
-          url: "https://api.anonfiles.com/upload",
-          formData: {
-            file: fs.createReadStream(`nhentai/${title}.pdf`),
-          },
-        }
-
-        request(options, function (err, res, body) {
-          if (err) console.log(err);
-          fs.unlink(`nhentai/${title}.pdf`, (err) => {
-            if (err) throw err
-          })
-          lintod.reply("Link download to file: " + JSON.parse(body).data.file.url.full);
-        })
-      }
-    } catch (error) {
-      lintod.reply(String(error));
-      console.log("No Dōjinshi found");
-      console.log(error);
-    }
-  } else if (body && !body.match(/^[0-9]/)) {
-  	simih = await axios.get("https://zenzapi.xyz/api/simih?text=" + encodeURI(body) + "&apikey=Nyarlathotep")
-  await lintod.reply(simih.data.result.message);
+  if (body && !body.match(/^[0-9]/)) {
+  simih = await axios.get("https://zenzapi.xyz/api/simih?text=" + encodeURI(body) + "&apikey=Nyarlathotep")
+  await nino.reply(simih.data.result.message);
   }
 })
 
